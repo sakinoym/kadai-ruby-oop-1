2268188289b4a704c5136441d45aa56e015e21dc
saki.y <sakino.ym@gmail.com>
2022-03-12 13:31:21 +0000

    third

--- team.rb
+++ team.rb
@@ -2,35 +2,13 @@ class Team
 
   attr_accessor :name, :win, :lose, :draw
 
-  def initialize(name)
+  def initialize(name,win,lose,draw)
     self.name = name
-    if name == "Giants"
-      self.win = 67
-      self.lose = 45
-      self.draw = 8
-    elsif name == "Tigers"
-      self.win = 60
-      self.lose = 53
-      self.draw = 7
-    elsif name == "Dragons"
-      self.win = 60
-      self.lose = 55
-      self.draw = 5
-    elsif name == "BayStars"
-      self.win = 56
-      self.lose = 58
-      self.draw = 6
-    elsif name == "Carp"
-      self.win = 52
-      self.lose = 56
-      self.draw = 12
-    else
-      self.win = 41
-      self.lose = 69
-      self.draw = 10
-    end
+    self.win = win
+    self.lose = lose
+    self.draw = draw
   end
-
+   
   def calc_win_rate
     self.win.to_f / (self.win + self.lose)
   end
@@ -40,20 +18,20 @@ class Team
   end
 end
 
-Team1 = Team.new("Giants")
+Team1 = Team.new("Giants",67,45,8)
 Team1.show_team_result
 
-Team2 = Team.new("Tigers")
+Team2 = Team.new("Tigers",60,53,7)
 Team2.show_team_result
 
-Team3 = Team.new("Dragons")
+Team3 = Team.new("Dragons",60,55,5)
 Team3.show_team_result
 
-Team4 = Team.new("BayStars")
+Team4 = Team.new("BayStars",56,58,6)
 Team4.show_team_result
 
-Team5 = Team.new("Carp")
+Team5 = Team.new("Carp",52,56,12)
 Team5.show_team_result
 
-Team6 = Team.new("Swallows")
+Team6 = Team.new("Swallows",41,69,10)
 Team6.show_team_result
